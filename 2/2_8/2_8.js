ebd6ca165b9b0128358aa7bb8e4fab9bdcc3c992
Melee <melee.gods@mail.ru>
2024-05-13 15:17:09 +0300

    Дорешал до 2_10

--- 2/2_8/2_8.js
+++ 2/2_8/2_8.js
@@ -1,1 +1,44 @@
 'use strict';
+
+// Дана строка с буквами. Проверьте, что в этой строке не более двух заглавных букв.
+function task_2_8_1() {
+    let str = prompt('Введите строку:');
+    if (str === null) {
+      return null;
+    }
+  
+    let a = 0;
+    for (let i = 0; i < str.length; i++) {
+      let char = str[i];
+      if (char === char.toUpperCase() && char !== char.toLowerCase()) {
+        a++;
+      }
+    }
+    return a <= 2;
+  }
+  console.log(task_2_8_1()); // выведет true, если заглавных букв не более двух, иначе false
+
+
+let string = '1 22 333 4444 22 5555 1';
+// Дана некоторая строка: '1 22 333 4444 22 5555 1' Удалите из этой строки все подстроки, в которых количество символов больше трех. В нашем случае должно получится следующее: '1 22 333 22 1'
+function task_2_8_2(input) {
+    return input.split(' ').filter(word => word.length <= 3).join(' ');
+  }
+  console.log(task_2_8_2(string));
+
+
+let arr_1 = [1, 2, 3]
+let arr_2 = ['a', 'b', 'c']
+// Даны два массива:let arr1 = [1, 2, 3]; let arr2 = ['a', 'b', 'c']; Слейте эти массивы в новый массив следующим образом: [1, 2, 'a', 'b', 'c', 3]
+function task_2_8_3(arr_1, arr_2) {
+    let arr = arr_1.slice();//копия первого массива, чтобы не изменять исходный
+  
+    let lastElement = arr.pop();// убрал последний элемент из копии и сохранил его
+  
+    arr = arr.concat(arr_2);// объединил копию с вторым массивом
+  
+    arr.push(lastElement);// добавил  обратно в конец нового массива
+  
+    return arr;
+  }
+  console.log(task_2_8_3(arr_1, arr_2));
--- 2/2_9/2_9.js
+++ 2/2_9/2_9.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// Дано некоторое число: 123456 Найдите сумму пар цифр этого числа. В нашем случае имеется ввиду следующее:12 + 34 + 56
+function task_2_9_1(number) {
+    let sum = 0;
+    let numberStr = number.toString();
+  
+    for (let i = 0; i < numberStr.length; i += 2) {
+      sum += parseInt(numberStr[i] + numberStr[i + 1]);
+    }
+  
+    return sum;
+  }
+  
+  console.log(task_2_9_1(123456));
+
+
+//   Дан массив с числами:[1, 2, 3, 4, 5] Выведите в консоль элементы этого массива в обратном порядке.
+  function task_2_9_2(arr) {
+    let a = arr.reverse(); 
+    console.log(a);
+  }
+task_2_9_2([1, 2, 3, 4, 5]);
